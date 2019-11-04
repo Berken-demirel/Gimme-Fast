c157e9a6ab592c5dff7d172305ccac6f363dbe7b
Berken Demirel <54396476+Berken-demirel@users.noreply.github.com>
2019-11-04 20:26:48 +0300

    Update Butterworth_bandpass_ex.m

--- Matlab/Butterworth_bandpass_ex.m
+++ Matlab/Butterworth_bandpass_ex.m
@@ -1,6 +1,6 @@
  f1 = 2;  % cuttoff low frequency to get rid of baseline wander                                                                 
  f2 = 30; % cuttoff frequency to discard high frequency noise
- Fs = 329; % cuttoff frequency to discard high frequency noise
+ Fs = 329; % Sampling Rate
  Wn = [f1 f2] * 2 / Fs ;                    
  N = 3;     % order of 3 less processing                                                               
  [b, a] = butter(N,Wn,'bandpass'); %Butterworth filter design 
